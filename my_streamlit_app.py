8d8e67833efbb8971762b3583ce0d5a5beab7aec
Halil Coban <halil.coban@telekom.de>
2022-06-01 08:44:32 +0200

    Still working on the histogramm

--- my_streamlit_app.py
+++ my_streamlit_app.py
@@ -1,25 +1,41 @@
 import streamlit as st
 import pandas as pd
 import seaborn as sns
+import matplotlib.pyplot as plt
 
-st.title('Hello Wilders, welcome to my application!')
-
-st.write("I enjoy to discover stremalit possibilities")
-
-link = "https://raw.githubusercontent.com/murpi/wilddata/master/quests/weather2019.csv"
-df_weather = pd.read_csv(link)
-
-df_weather
-
-st.line_chart(df_weather['MAX_TEMPERATURE_C'])
+st.title('Hello Wilders, welcome to my Car application!')
 
 
 
-viz_correlation = sns.heatmap(df_weather.corr(),
+link = "https://raw.githubusercontent.com/murpi/wilddata/master/quests/cars.csv"
+df_car = pd.read_csv(link)
+
+
+st.write("Here the pure car data:")
+
+df_car
+
+# st.line_chart(df_weather['MAX_TEMPERATURE_C'])
+
+
+
+
+st.write("Here the correlation heatmap:")
+
+viz_correlation = sns.heatmap(df_car.corr(),
     center=0,
     cmap = sns.color_palette("vlag", as_cmap=True)
     )
 st.pyplot(viz_correlation.figure)
+
+st.write("Here the distribution of the car dataframe:")
+
+df_car.hist()
+
+fig, ax = plt.subplots(3, 3, tight_layout=True)
+plt.show()
+st.pyplot(fig)
+
 
 name = st.text_input("Please give me your name:")
 name_length = len(name)
